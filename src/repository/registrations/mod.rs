ce2cbbbba0528e6486389ad212e4e0f7377b4cb3
Anders Borch <anders@cyborch.com>
2023-06-02 14:03:07 +0200

    Remove dead code

--- src/repository/registrations/mod.rs
+++ src/repository/registrations/mod.rs
@@ -1,11 +1,1 @@
-use sqlx::{Error, Pool, Postgres};
-
-use self::models::*;
-
 pub mod models;
-
-pub async fn find_all(pool: &Pool<Postgres>) -> Result<Vec<Registration>, Error> {
-    sqlx::query_as!(Registration, "select * from registrations")
-        .fetch_all(pool)
-        .await
-}
