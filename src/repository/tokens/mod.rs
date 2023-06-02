f429a714a43c4e5b833af61c8c0164bd7d0ea61d
Anders Borch <anders@cyborch.com>
2023-06-02 14:33:54 +0200

    Split json deserializer into separate function

--- src/repository/tokens/mod.rs
+++ src/repository/tokens/mod.rs
@@ -4,6 +4,7 @@ use sqlx::types::JsonRawValue;
 use sqlx::{postgres::PgRow, FromRow, Pool, Postgres, Row};
 
 use self::models::*;
+use super::registrations::models::*;
 
 pub mod models;
 
@@ -23,20 +24,35 @@ pub fn find_all(pool: &Pool<Postgres>) -> impl Stream<Item = Token> {
     .map(|result| result.expect("Token::from_row error"))
 }
 
+fn default_json() -> Box<JsonRawValue> {
+    JsonRawValue::from_string(String::from("{}")).ok().unwrap()
+}
+
+fn raw_value<'a>(row: &'a PgRow, key: &str) -> Option<&'a JsonRawValue> {
+    row.try_get(key).ok()
+}
+
+fn result(row: &PgRow) -> Result<Registration, serde_json::Error> {
+    let default = default_json();
+    let json = raw_value(&row, "registration").unwrap_or(&default);
+    // FIXME: skip the to_string middleman
+    serde_json::from_str(json.to_string().as_str())
+}
+
 impl FromRow<'_, PgRow> for Token {
     fn from_row(row: &PgRow) -> sqlx::Result<Self> {
-        let json: &JsonRawValue = row.try_get("registration")?;
-        // FIXME: skip the to_string middleman
-        let registration = serde_json::from_str(json.to_string().as_str()).unwrap();
-        Ok(Self {
-            id: row.try_get("id")?,
-            username: row.try_get("username")?,
-            access_token: row.try_get("access_token")?,
-            token_type: row.try_get("token_type")?,
-            scope: row.try_get("scope")?,
-            created_at: row.try_get("created_at")?,
-            fail_count: row.try_get("fail_count")?,
-            registration,
-        })
+        match result(&row) {
+            Ok(registration) => Ok(Self {
+                id: row.try_get("id")?,
+                username: row.try_get("username")?,
+                access_token: row.try_get("access_token")?,
+                token_type: row.try_get("token_type")?,
+                scope: row.try_get("scope")?,
+                created_at: row.try_get("created_at")?,
+                fail_count: row.try_get("fail_count")?,
+                registration,
+            }),
+            Err(error) => Err(sqlx::Error::ColumnNotFound(error.to_string())),
+        }
     }
 }
