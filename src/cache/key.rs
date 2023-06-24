8a3ccbad0d854afb929ee76dc1307cc82248f123
Anders Borch <anders@cyborch.com>
2023-06-24 18:37:01 +0200

    v2 cache keys

--- src/cache/key.rs
+++ src/cache/key.rs
@@ -1,11 +1,7 @@
 pub fn status_key(instance_url: &String, status_url: &String) -> String {
-    format!("{}:{}", instance_url, status_url)
-}
-
-pub fn instance_key(instance_url: &String) -> String {
-    instance_url.to_string()
+    format!("v2:{}:{}", instance_url, status_url)
 }
 
 fn follow_key(username: &String) -> String {
-    format!("{}:following", username)
+    format!("v2:{}:following", username)
 }
