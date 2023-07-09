26c4b75f3781b9780e012f2d0e5a7372bcd2cd92
Anders Borch <anders@cyborch.com>
2023-07-09 16:17:03 +0200

    Added fetch_contexts_for_tokens_loop and verify_token

--- src/run_loop.rs
+++ src/run_loop.rs
@@ -4,8 +4,12 @@ use futures_util::StreamExt;
 use tokio::sync::Mutex;
 
 use crate::{
-    context, home, notification, prepare,
-    repository::{self, tokens, Connection, ConnectionPool},
+    context, federated, home, notification, prepare,
+    repository::{
+        self,
+        tokens::{self, Token},
+        Connection, ConnectionPool,
+    },
 };
 
 fn worker_id() -> i32 {
@@ -33,6 +37,16 @@ fn process_interval() -> Duration {
     )
 }
 
+/// If requests with a token fails for more than 10 minutes
+/// (1 request every 2 seconds) then assume that it has been
+/// revoked and delete it.
+fn max_fail_count() -> i32 {
+    env::var("TOKEN_MAX_FAIL_COUNT")
+        .unwrap_or("300".to_owned())
+        .parse::<i32>()
+        .unwrap_or(60 * 5)
+}
+
 async fn connect(db: Arc<Mutex<ConnectionPool>>) -> Result<Connection, String> {
     let db = db.lock().await;
     repository::connect(&db)
@@ -40,16 +54,51 @@ async fn connect(db: Arc<Mutex<ConnectionPool>>) -> Result<Connection, String> {
         .map_err(|err| err.to_string())
 }
 
-async fn fetch_contexts_for_tokens_loop(db: Arc<Mutex<ConnectionPool>>) {
-    if let Ok(mut connection) = connect(db).await {
-        loop {
-            tokens::find_by_worker_id(&mut connection, worker_id())
-                .for_each_concurrent(None, |token| async move {
+/// Verify token
+///
+/// Check that a token can be used to create an authenticated client
+/// and update the fail count. Delete tokens whose fail count goes
+/// over the set threshold.
+async fn verify_token(token: &Token, db: Arc<Mutex<ConnectionPool>>) -> Result<(), String> {
+    match connect(db).await {
+        Ok(mut connection) => {
+            let result = federated::has_verified_authenticated_client(token).await;
+            if result.is_ok() {
+                repository::tokens::update_fail_count(&mut connection, token, 0).await?;
+            } else {
+                let fail_count = token.fail_count.unwrap_or(0) + 1;
+                if fail_count > max_fail_count() {
                     println!(
-                        "fetch_contexts_for_tokens_loop got token for: {:?}",
+                        "Fail-count threshold exceeded for {:?}, deleting",
                         token.username
                     );
-                    _ = context::fetch_next_context(&token).await;
+                    repository::tokens::delete(&mut connection, token).await?;
+                } else {
+                    repository::tokens::update_fail_count(&mut connection, token, fail_count)
+                        .await?;
+                }
+            }
+            result
+        }
+        Err(err) => Err(err),
+    }
+}
+
+async fn fetch_contexts_for_tokens_loop(db: Arc<Mutex<ConnectionPool>>) {
+    if let Ok(mut connection) = connect(db.clone()).await {
+        loop {
+            tokens::find_by_worker_id(&mut connection, worker_id())
+                .for_each_concurrent(None, |token| {
+                    let db = db.clone();
+                    async move {
+                        if verify_token(&token, db).await.is_ok() {
+                            println!(
+                                "fetch_contexts_for_tokens_loop got token for: {:?}",
+                                token.username
+                            );
+                            _ = context::fetch_next_context(&token).await;
+                        }
+                    }
                 })
                 .await;
             tokio::time::sleep(process_interval()).await;
@@ -58,18 +107,23 @@ async fn fetch_contexts_for_tokens_loop(db: Arc<Mutex<ConnectionPool>>) {
 }
 
 async fn queue_statuses_for_timelines(db: Arc<Mutex<ConnectionPool>>) {
-    if let Ok(mut connection) = connect(db).await {
+    if let Ok(mut connection) = connect(db.clone()).await {
         loop {
             tokens::find_by_worker_id(&mut connection, worker_id())
-                .for_each_concurrent(None, |token| async move {
-                    println!(
-                        "queue_statuses_for_timelines got token for: {:?}",
-                        token.username
-                    );
-                    let queue_name = &token.username;
-                    _ = prepare::prepare_populate_queue(queue_name).await;
-                    _ = home::queue_home_statuses(&token).await;
-                    _ = notification::resolve_notification_account_statuses(&token).await;
+                .for_each_concurrent(None, |token| {
+                    let db = db.clone();
+                    async move {
+                        if verify_token(&token, db).await.is_ok() {
+                            println!(
+                                "queue_statuses_for_timelines got token for: {:?}",
+                                token.username
+                            );
+                            let queue_name = &token.username;
+                            _ = prepare::prepare_populate_queue(queue_name).await;
+                            _ = home::queue_home_statuses(&token).await;
+                            _ = notification::resolve_notification_account_statuses(&token).await;
+                        }
+                    }
                 })
                 .await;
             println!(
