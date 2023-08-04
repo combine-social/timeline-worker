5b18b2c3c55fd65a2ab40cbb5fd631b70b81f9b5
Anders Borch <anders@cyborch.com>
2023-08-04 14:51:17 +0200

    Ignore 401 unauthorized get_context responses

--- src/federated/context.rs
+++ src/federated/context.rs
@@ -2,6 +2,8 @@ use megalodon::{
     entities::Context, megalodon::GetStatusContextInputOptions, response::Response, SNS,
 };
 
+use crate::strerr::here;
+
 use super::{client, throttle};
 
 fn context_options() -> Option<&'static GetStatusContextInputOptions> {
@@ -21,19 +23,43 @@ fn unwrap_context(
         Err(result.err().unwrap())
     }
 }
+
 pub async fn get_context(
     instance_url: &String,
     status_id: &str,
     sns: Option<&SNS>,
 ) -> Result<Option<Context>, String> {
     let rpm = 7500 / 5;
-    throttle::throttled(instance_url, Some(rpm), || async {
+    let result = throttle::throttled(instance_url, Some(rpm), || async {
         unwrap_context(
             client::anonymous_client(instance_url, sns.cloned())
                 .get_status_context(status_id.to_owned(), context_options())
                 .await,
         )
     })
-    .await
-    .map_err(|error| error.to_string())
+    .await;
+    if let Err(error) = result {
+        match error {
+            megalodon::error::Error::OwnError(ref own_err) => {
+                if own_err.status == Some(401) {
+                    warn!(
+                        "Authentication required for {}#{}, ignoring",
+                        instance_url, status_id
+                    );
+                    Ok(None)
+                } else {
+                    Err(here!(error))
+                }
+            }
+            _ => {
+                error!(
+                    "Failed getting context for {:?}#{:?}: {:?}",
+                    instance_url, status_id, error
+                );
+                Err(here!(error))
+            }
+        }
+    } else {
+        result.map_err(|err| here!(err))
+    }
 }
