c7c24a47cff8572b237be96ebd6a28e703af39a4
Anders Borch <anders@cyborch.com>
2023-08-07 08:23:39 +0200

    Sleep 5 minutes if rate limit is hit

--- src/federated/client.rs
+++ src/federated/client.rs
@@ -1,4 +1,6 @@
-use megalodon::{Megalodon, SNS};
+use std::time::Duration;
+
+use megalodon::{error::Kind, Megalodon, SNS};
 
 use crate::{repository::tokens::Token, strerr::here};
 
@@ -22,15 +24,37 @@ pub fn anonymous_client(url: &str, sns: Option<SNS>) -> Box<dyn Megalodon + Send
 
 pub async fn has_verified_authenticated_client(token: &Token) -> Result<(), String> {
     let client = authenticated_client(token);
-    client
+    let result = client
         .verify_account_credentials()
         .await
-        .map_err(|err| here!(err))
         .and_then(|response| {
             if response.json().id.is_empty() {
-                Err("Missing client id".to_owned())
+                Err(megalodon::error::Error::new_own(
+                    "Missing client id".to_string(),
+                    Kind::ParseError,
+                    Some(format!(
+                        "{}/api/v1/accounts/verify_credentials",
+                        &token.registration.instance_url
+                    )),
+                    None,
+                ))
             } else {
                 Ok(())
             }
-        })
+        });
+    if let Err(error) = &result {
+        match error {
+            megalodon::error::Error::OwnError(ref own_err) => {
+                warn!(
+                    "Rate limit exceeded for {}, sleeping 5 minutes",
+                    &token.username
+                );
+                if own_err.status == Some(429) {
+                    tokio::time::sleep(Duration::from_secs(300)).await;
+                }
+            }
+            _ => warn!("has_verified_authenticated_client error: {:?}", error),
+        }
+    }
+    result.map_err(|err| here!(err))
 }
