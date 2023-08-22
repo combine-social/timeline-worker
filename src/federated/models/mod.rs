7104a474837259d4bdc4afc5576378d3bb9ff7bf
Anders Borch <anders@cyborch.com>
2023-08-22 10:56:15 +0200

    Resolve mentioned account statuses in separate thread

--- src/federated/models/mod.rs
+++ src/federated/models/mod.rs
@@ -1,5 +1,6 @@
 pub mod activitypub;
 
+#[derive(Clone)]
 pub struct Page<T> {
     pub items: Vec<T>,
     pub(crate) max_id: Option<String>,
--- src/home.rs
+++ src/home.rs
@@ -1,23 +1,31 @@
 use megalodon::entities::Status;
 
 use crate::{
-    federated::{self},
+    federated::{self, Page},
     queue_statuses,
     repository::tokens::Token,
 };
+
+async fn resolve_page_mentioned_account_statuses(token: Token, page: Page<Status>) {
+    for status in page.items.iter() {
+        _ = resolve_mentioned_account_statuses(&token, status)
+            .await
+            .map_err(|err| {
+                error!("resolve_mentioned_account_statuses error: {:?}", err);
+                err
+            });
+    }
+}
 
 pub async fn queue_home_statuses(token: &Token) -> Result<(), String> {
     queue_statuses::queue_statuses(token, |max_id| async move {
         let page = federated::get_home_timeline_page(token, max_id).await?;
         info!("page has {:?} statuses", page.items.len());
-        for status in page.items.iter() {
-            resolve_mentioned_account_statuses(token, status)
-                .await
-                .map_err(|err| {
-                    error!("resolve_mentioned_account_statuses error: {:?}", err);
-                    err
-                })?;
-        }
+        let token = token.to_owned();
+        let copy = page.clone();
+        tokio::spawn(async move {
+            resolve_page_mentioned_account_statuses(token, copy).await;
+        });
         Ok(page)
     })
     .await
