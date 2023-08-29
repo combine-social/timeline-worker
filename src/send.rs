e1846412db9ad5bb47974c92f905a79d20018d08
Anders Borch <anders@cyborch.com>
2023-08-29 16:57:34 +0200

    Added MAX_QUEUE_SIZE configuration option

--- src/send.rs
+++ src/send.rs
@@ -1,11 +1,21 @@
+use std::env;
+
 use chrono::Utc;
 
 use crate::{
     cache::{self, StatusCacheMetaData},
     conditional_queue,
     models::ContextRequest,
+    queue,
     repository::tokens::Token,
 };
+
+fn max_queue_size() -> u32 {
+    env::var("MAX_QUEUE_SIZE")
+        .unwrap_or("1000".to_owned())
+        .parse()
+        .unwrap_or(1000)
+}
 
 fn next_level(meta: &StatusCacheMetaData) -> StatusCacheMetaData {
     StatusCacheMetaData {
@@ -26,6 +36,12 @@ pub async fn send_if_needed(
     let mut cache = cache::connect().await?;
     let own_instance = &token.registration.instance_url;
     let queue_name = format!("v2:{}", token.username);
+
+    let queue_size = queue::size(&queue_name).await?;
+    if queue_size >= max_queue_size() {
+        warn!("Max queue size exceeded for {}, bailing.", queue_name);
+        return Ok(());
+    }
 
     _ = conditional_queue::send_if_not_cached(
         &mut cache,
